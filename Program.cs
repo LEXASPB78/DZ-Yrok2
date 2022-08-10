596078ce20020494ecccc5dd523e5065b59d664c
LEXASPB78 <ashkreba@yandex.ru>
2022-08-10 19:26:33 +0300

    Решение10

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string stringnum = Convert.ToString(number);
+Console.WriteLine(stringnum[1]);
